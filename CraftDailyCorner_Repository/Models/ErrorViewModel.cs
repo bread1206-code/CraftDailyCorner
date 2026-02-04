c5e43b14ecc413053851e133e28cd0d4a884f7d3
bread1206 <bread1206@gmail.com>
2026-02-04 08:36:22 +0800

    刪除不使用的專案

--- CraftDailyCorner_Repository/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CraftDailyCorner_Repository.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
