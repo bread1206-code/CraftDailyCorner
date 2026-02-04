7eca9150f2215e7444a89dcd0c26ecad1c21aab1
bread1206 <bread1206@gmail.com>
2026-02-04 12:51:14 +0800

    暫存變更

--- CraftDailyCorner/Controllers/ProductsController.cs
+++ CraftDailyCorner/Controllers/ProductsController.cs
@@ -14,6 +14,52 @@ namespace CraftDailyCorner.Controllers
         {
             _context = context;
         }
+        public IActionResult Index(
+            int? categoryId,
+            string? keyword,
+            int? tagId
+            )
+        {
+            var query = _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                    .ThenInclude(pc => pc.Category)
+                .Include(p => p.ProductTags)
+                    .ThenInclude(pt => pt.Tag)
+                .Where(p => p.StatusID == 2); // 上架商品
+
+            // 1️ 分類瀏覽
+            if (categoryId.HasValue)
+            {
+                query = query.Where(p =>
+                    p.ProductCategories.Any(pc => pc.CategoryID == categoryId));
+            }
+
+            // 2️ 關鍵字搜尋
+            if (!string.IsNullOrWhiteSpace(keyword))
+            {
+                query = query.Where(p =>
+                    p.ProductName.Contains(keyword) ||
+                    p.Description.Contains(keyword));
+            }
+
+            // 3 Tag 瀏覽
+            if (tagId.HasValue)
+            {
+                query = query.Where(p =>
+                    p.ProductTags.Any(pt => pt.TagID == tagId));
+            }
+
+            var vm = new VMProductList
+            {
+                Products = query.ToList(),
+                CategoryId = categoryId,
+                Keyword = keyword,
+                TagId = tagId
+            };
+
+            return View(vm);
+        }
         public IActionResult Detail(string id)
         {
             var product = _context.Products
---
+++ CraftDailyCorner/ViewModels/Front/VMProductList.cs
@@ -0,0 +1,15 @@
+﻿using CraftDailyCorner.Models;
+
+namespace CraftDailyCorner.ViewModels.Front
+{
+    public class VMProductList
+    {
+        public List<Product> Products { get; set; } = new();
+        public List<ProductImage> Images { get; set; } = null!;
+
+        // 條件回填用（之後前端很好做）
+        public int? CategoryId { get; set; }
+        public string? Keyword { get; set; }
+        public int? TagId { get; set; }
+    }
+}
